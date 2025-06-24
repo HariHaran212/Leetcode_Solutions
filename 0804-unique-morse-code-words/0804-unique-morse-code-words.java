a721791c18f5335a865c2f3602f939fd0b1b8412
Hariharan <137514897+HariHaran212@users.noreply.github.com>
2025-06-24 13:40:00 +0530

    Time: 1 ms (100%), Space: 41.5 MB (79.27%) - LeetHub

--- 0804-unique-morse-code-words/0804-unique-morse-code-words.java
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.java
@@ -1,24 +1,20 @@
 class Solution {
-    private String toMorse(Map<Character, String> map, String word){
+    private String toMorse(String[] str, String word){
         StringBuilder sb = new StringBuilder();
 
         for(char ch : word.toCharArray()){
-            sb.append(map.get(ch));
+            sb.append(str[ch-'a']);
         }
 
         return sb.toString();
     }
     public int uniqueMorseRepresentations(String[] words) {
-        Map<Character, String> map = new HashMap<>();
         String[] str = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        for(int i = 0 ; i<26 ; i++){
-            map.put((char)('a'+i), str[i]);
-        }
 
         Set<String> set = new HashSet<>();
 
         for(int i = 0 ; i<words.length ; i++){
-            String s = toMorse(map, words[i]);
+            String s = toMorse(str, words[i]);
             set.add(s);
         }
 
