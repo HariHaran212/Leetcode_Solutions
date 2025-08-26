368b93b39b2885d58c13802ec1b9d7d07be0f0fb
Hariharan <137514897+HariHaran212@users.noreply.github.com>
2025-08-26 14:34:59 +0530

    Time: 1 ms (99.52%), Space: 56.7 MB (89.66%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,7 +3,12 @@ class Solution {
         int maxSum = Integer.MIN_VALUE;
         int currSum = 0;
         for(int num : nums){
-            currSum = num < currSum+num ? currSum += num : num;
+            if(num < currSum+num){
+                currSum += num;
+            }
+            else{
+                currSum = num;
+            }
             if(currSum > maxSum){
                 maxSum = currSum;
             }
