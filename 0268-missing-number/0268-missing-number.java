db3d905fd07a285592b4ecd63645ec532bda2e72
Hariharan <137514897+HariHaran212@users.noreply.github.com>
2025-09-26 12:08:04 +0530

    Time: 0 ms (100%), Space: 45.3 MB (56.9%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,14 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        int prev = 0;
+        int xor = nums.length;
+
         for(int i = 0 ; i<nums.length ; i++){
-            if(nums[i] != prev){
-                return prev;
-            }
-            prev = nums[i]+1;
+            xor^=(nums[i]^i);
         }
 
-        return prev;
+        return xor;
     }
 }
